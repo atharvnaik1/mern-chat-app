803ed646e82ce98d879ac9828f560e8873f2b89d
atharvnaik1 <atharv1599@gmail.com>
2024-03-04 00:39:45 +0530

    issue solved

--- backend/db/db.js
+++ backend/db/db.js
@@ -6,7 +6,7 @@ const connectDB = async () =>{
 
   try {
 
-    await mongoose.connect(process.env.MONGODB_URL)
+    await mongoose.connect(process.env.MONGODB_URI)
     console.log("Connected to MongoDB")
     
   } catch (error) {
